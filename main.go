95a7639fedafccf8eb046b01c6c8a62a31aeb43f
Amarachi Iheanacho <amarachi2812@gmail.com>
2024-01-09 17:56:57 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//strings
+	var nameOne string = "amarachi"
+	var nameTwo = "ugochi"
+
+	nameFour := "yoshi"
+	fmt.Println(nameOne, nameTwo, nameFour)
+
+	//integer
+
+	// int is for whole numbers
+	var ageOne int = 16
+	ageTwo := 22
+	fmt.Println(ageOne, ageTwo)
+
+	//specify bits size
+	// bits & memory
+
+	// var numOne int8 = 25
+	// var numTwo int8 = -128
+	// var numThree uint16 = 256 
+
+	//float for decimal
+
+	var scoreOne float32 = 25.98
+	var scoreTwo float64 = 4445469609240345.123
+
+	scoreThree := 1.5
+
+	fmt.Println(scoreOne, scoreTwo, scoreThree)
+
+}
