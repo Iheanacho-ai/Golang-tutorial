5906c2ae0cf2a9abb3222646cd60c7a97fc50df3
Amarachi Iheanacho <amarachi2812@gmail.com>
2024-01-09 18:42:36 +0100

    added new tutorial learnings

--- main.go
+++ main.go
@@ -3,35 +3,29 @@ package main
 import "fmt"
 
 func main() {
+	age:= 35
+	name:= "amarachi"
 
-	//strings
-	var nameOne string = "amarachi"
-	var nameTwo = "ugochi"
+	//print
+	fmt.Print("hello,")
+	fmt.Print("world! \n") //new line
+	fmt.Print("new line \n")
 
-	nameFour := "yoshi"
-	fmt.Println(nameOne, nameTwo, nameFour)
+	//Println
+	fmt.Println("hello world")
+	fmt.Println("goodbye world")
 
-	//integer
+	fmt.Println("my name is", name, "and i am", age)
 
-	// int is for whole numbers
-	var ageOne int = 16
-	ageTwo := 22
-	fmt.Println(ageOne, ageTwo)
+	//Printf(formatted strings)
+	fmt.Printf("my age is %v and my name is %v", age, name)
+	fmt.Printf("my age is %q and my name is %q", age, name) // puts quotation over the printed out string
+	fmt.Printf("age is of type %T \n", age)// prints out the type of the age variable
+	fmt.Printf("you scored %f points! \n", 225.55) //prints out the text with the float number
+	fmt.Printf("you scored %0.1f points! \n", 225.55) // prints out the text with the float number but rounds it up to one decimal points
 
-	//specify bits size
-	// bits & memory
-
-	// var numOne int8 = 25
-	// var numTwo int8 = -128
-	// var numThree uint16 = 256 
-
-	//float for decimal
-
-	var scoreOne float32 = 25.98
-	var scoreTwo float64 = 4445469609240345.123
-
-	scoreThree := 1.5
-
-	fmt.Println(scoreOne, scoreTwo, scoreThree)
+	//Sprintf( save formatted string)
+	var str = fmt.Sprintf("my age is %v and my name is %v", age, name)
+	fmt.Println("the saved string is:", str)
 
 }
