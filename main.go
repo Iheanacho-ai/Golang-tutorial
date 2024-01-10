a8c206055424e9831d107599ac01f473a3ecf4f5
Amarachi Iheanacho <amarachi2812@gmail.com>
2024-01-10 19:52:31 +0100

    added pass-by-values branch

--- main.go
+++ main.go
@@ -5,46 +5,34 @@ import (
 	"fmt"
 )
 
+func updateName(x string) string {
+	x = "wedge"
+	return x
+}
 
+func updateMenu(y map[string]float64) {
+	y["coffee"] = 2.99
+}
 
 func main() {
-	menu:= map[string]float64{
-		"soup": 4.99,
-		"pie": 7.99,
-		"salad": 6.99,
-		"toffee pudding": 3.55,
+	// group A types -> strings, ints, bools, floats, arrays -> non-pointer values
+
+	name := "tifa"
+	name = updateName(name)
+	fmt.Println(name)
+
+	// group B -> slices, maps, functions -> pointer wrapper values
+	menu := map[string]float64{
+		"pie": 5.95,
+		"ice cream": 3.99,
 	}
 
+	updateMenu(menu)
 	fmt.Println(menu)
-	fmt.Println(menu["soup"])
 
-	// looping through maps
 
-	for k, v := range menu {
-		fmt.Println(k, "-", v)
-	}
 
-	//ints as key types
-	phonebook:= map[int]string{
-		2453367834: "mario",
-		4567234478: "luigi",
-		2133456789: "peach", 
-	}
 
-	fmt.Println(phonebook)
-	fmt.Println(phonebook[2453367834])
-
-	for k, v := range phonebook {
-		fmt.Println(k, "-", v)
-	}
-
-	//update maps
-
-	phonebook[2133456789] = "bowser"
-	fmt.Println(phonebook);
-
-	phonebook[4567234478] = "yoshi"
-	fmt.Println(phonebook);
 
 }
 
