9d1e88cf1100b254c1e92217470be175d47596f5
Amarachi Iheanacho <amarachi2812@gmail.com>
2024-01-10 13:43:46 +0100

    maps

--- main.go
+++ main.go
@@ -5,18 +5,47 @@ import (
 	"fmt"
 )
 
-var score = 99.5
+
 
 func main() {
-	fmt.Println("yeah yeah")
-
-	sayHello("Amarachi")
-	
-	for _, v := range points {
-		fmt.Println(v)
+	menu:= map[string]float64{
+		"soup": 4.99,
+		"pie": 7.99,
+		"salad": 6.99,
+		"toffee pudding": 3.55,
 	}
 
-	showScore()
+	fmt.Println(menu)
+	fmt.Println(menu["soup"])
+
+	// looping through maps
+
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
+
+	//ints as key types
+	phonebook:= map[int]string{
+		2453367834: "mario",
+		4567234478: "luigi",
+		2133456789: "peach", 
+	}
+
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[2453367834])
+
+	for k, v := range phonebook {
+		fmt.Println(k, "-", v)
+	}
+
+	//update maps
+
+	phonebook[2133456789] = "bowser"
+	fmt.Println(phonebook);
+
+	phonebook[4567234478] = "yoshi"
+	fmt.Println(phonebook);
+
 }
 
 
