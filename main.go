80c7ccfd1baab7c36a4dbf127ce67b4837f9eecf
Amarachi Iheanacho <amarachi2812@gmail.com>
2024-01-10 13:32:57 +0100

    added package-scope branch

--- main.go
+++ main.go
@@ -3,34 +3,20 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func getInitials (n string) (string, string){
-	s := strings.ToUpper(n) // TIFA LOCKHART
-	names := strings.Split(s, "") // [TIFA, LOCKHART]
-
-	var initials []string // {}
-	for _, v := range names {
-		initials = append(initials, v[:1]) // appends the items in postion 0 of the array value currently worked on in the loop, through to position 1, does not include 1 though, to the initials array
-	}
-
-	if len(initials) > 1 {
-		return initials[0], initials[1]
-	} 
-	
-	return initials[0], "_"
-}
-
+var score = 99.5
 
 func main() {
-	fn1, sn1 := getInitials("tifa lockhart")
-	fmt.Println(fn1, sn1)
+	fmt.Println("yeah yeah")
 
-	fn2, sn2 := getInitials("cloud strife")
-	fmt.Println(fn2, sn2)
+	sayHello("Amarachi")
+	
+	for _, v := range points {
+		fmt.Println(v)
+	}
 
-	fn3, sn3 := getInitials("barret")
-	fmt.Println(fn3, sn3)
+	showScore()
 }
+
 
