f5da25a3db110ecbe7d344a4fa792e6eaee585b7
Amarachi Iheanacho <amarachi2812@gmail.com>
2024-01-10 20:41:48 +0100

    added the structs branch

--- main.go
+++ main.go
@@ -1,30 +1,14 @@
-
 package main
 
 import (
 	"fmt"
 )
 
-func updateName(x *string){ // we dereference the pointer passed as ana rgument
-	*x = "wedge" // we change the derefernced variable to a different string
-}
-
 
 func main() {
-	name := "tifa"
-	// updateName(name)
+	mybill := newBill("mario's bill")
 
-	//fmt.Println("memory address of name is:", &name)
-
-	m := &name // stores the pointer we created by using '&', and the variable name
-	fmt.Println("memory address is:", &name)
-	fmt.Println("value at memory address:", *m) // *m finds the variable using the pointer &name, basically dereferences the pointer
-
-	fmt.Println(name)
-	updateName(m)
-
-	fmt.Println(name)
-
+	fmt.Println(mybill)
 }
 
 
