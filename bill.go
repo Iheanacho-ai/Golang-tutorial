a47b3fd021ebb20014d031fbe01461ecb99e8e06
Amarachi Iheanacho <amarachi2812@gmail.com>
2024-01-11 12:56:45 +0100

    receiver functions with pointers

--- bill.go
+++ bill.go
@@ -20,7 +20,7 @@ func newBill(name string) bill {
 }
 
 // format the bill
-func (b bill) format() string { // receiver functions, the parenthesis before the function name, makes this function only accessible from a bill object
+func (b *bill) format() string { // receiver functions, the parenthesis before the function name, makes this function only accessible from a bill object
 	fs := "Bill breakdown: \n"
 	var total float64 = 0
 
@@ -30,8 +30,21 @@ func (b bill) format() string { // receiver functions, the parenthesis before th
 		total += v
 	}
 
+	//add tip
+	fs += fmt.Sprintf("%-25v ...$%v \n", "tip:", b.tip)
 	//total
-	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total)
+	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total+b.tip)
 
 	return fs
+}
+
+//update tip
+
+func (b *bill) updateTip(tip float64){ // when you pass a struct to a receiver function, go makes a copy, so when you try to change it, it only changes the copy, thats why you should make a copy of the pointer instead
+	b.tip = tip // however go dereferences the pointer automatically
+}
+
+// add an item to the bill
+func (b *bill) addItem (name string, price float64) {
+	b.items[name] = price
 }
--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	mybill := newBill("mario's bill")
 	// mybill.format() // we can call the format receiver function 
+	mybill.addItem("onion soup", 4.50)
+	mybill.updateTip(10)
 	fmt.Println(mybill.format())
 }
 
